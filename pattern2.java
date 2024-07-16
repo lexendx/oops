6cf1b4d1de873e51cb1d9fb150a5df57bccfe5a3
Baibhav Tiwari <97496657+lexendx@users.noreply.github.com>
2024-07-16 19:58:06 +0530

    Update pattern2.java

--- pattern2.java
+++ pattern2.java
@@ -1,14 +1,20 @@
-import java.util.*;
-public class pattern2 {
-    public static void main(String[] args) {
-        int m , n , rows;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("eneter the rows you want to print");
-        rows = sc.nextInt();
-        for(int i=1;i<rows;i++){
-for(j=1;j<=)
 
+import java.util.*;
+
+public class pattern2 {
+
+    public static void main(String[] args) {
+        int m, n, rows;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows you want to print");
+        rows = sc.nextInt();
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
-    
+
 }
+
